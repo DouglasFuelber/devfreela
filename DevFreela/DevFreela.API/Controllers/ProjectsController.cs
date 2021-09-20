cf495597ba77ce3067b65d9092003483fab04f9f
Douglas Fuelber <contato@douglasfuelber.com>
2021-09-20 20:09:01 -0300

    Created projects controller

---
+++ DevFreela/DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,40 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel project)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = project.Id }, project);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel project)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Detete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
