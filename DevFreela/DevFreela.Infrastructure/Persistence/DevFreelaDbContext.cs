561df24874821f7ea0d9ad211ae150598ff67925
Douglas Fuelber <contato@douglasfuelber.com>
2021-09-27 23:45:42 -0300

    Created Infrastructure project

---
+++ DevFreela/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DevFreela.Core.Entities;
+using System.Collections.Generic;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>();
+            Users = new List<User>();
+            Skills = new List<Skill>();
+        }
+        public List<Project> Projects{ get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
