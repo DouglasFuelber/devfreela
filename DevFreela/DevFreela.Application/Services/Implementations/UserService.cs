aefacac95287a038a11258c4228ae8cd829b6cfd
Douglas Fuelber <contato@douglasfuelber.com>
2021-10-02 14:19:25 -0300

    Added user service

---
+++ DevFreela/DevFreela.Application/Services/Implementations/UserService.cs
@@ -0,0 +1,45 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using System.Linq;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public UserService(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int Create(CreateUserInputModel inputModel)
+        {
+            User user = new User(
+                inputModel.Fullname, 
+                inputModel.Email,
+                inputModel.BirthDate);
+
+            _dbContext.Users.Add(user);
+
+            return user.Id;
+        }
+
+        public UserViewModel GetById(int id)
+        {
+            User user = _dbContext.Users.SingleOrDefault(p => p.Id == id);
+
+            if (user == null)
+                return null;
+
+            UserViewModel userViewModel = new UserViewModel(
+                user.FullName,
+                user.Email,
+                user.BirthDate);
+
+            return userViewModel;
+        }
+    }
+}
