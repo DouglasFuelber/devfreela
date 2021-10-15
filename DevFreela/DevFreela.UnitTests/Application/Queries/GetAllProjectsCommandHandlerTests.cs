9ad53dc225af33e98e74a98fcdf5371d8b990b36
DouglasFuelber <contato@douglasfuelber.com>
2021-10-15 16:55:48 -0300

    Added query test with mock

---
+++ DevFreela/DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async void ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
+        {
+            // Arrange
+            var projects = new List<Project> {
+                new Project("Test project 1", "Description of test project 1", 1, 2, 1000),
+                new Project("Test project 2", "Description of test project 2", 1, 2, 2000),
+                new Project("Test project 3", "Description of test project 3", 1, 2, 3000),
+            };
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(p => p.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjectsQuery = new GetAllProjectsQuery("");
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+
+            // Act
+            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList);
+            Assert.Equal(projects.Count, projectViewModelList.Count);
+
+            projectRepositoryMock.Verify(p => p.GetAllAsync().Result, Times.Once);
+        }
+    }
+}
